f30862daf1a05ecac3d335505e22808a70dbd1a9
BigObasheer <838@holbertonschool.com>
2020-02-07 00:34:54 +0000

    Add Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 9; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
