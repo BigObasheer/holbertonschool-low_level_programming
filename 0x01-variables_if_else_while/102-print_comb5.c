86303f3d33805534f32429871eadd7d05c13618c
BigObasheer <838@holbertonschool.com>
2020-02-07 03:36:50 +0000

    Add Task 13

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			if (i != 0 || j != 1)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			putchar('0' + i / 10);
+			putchar('0' + i % 10);
+			putchar(' ');
+			putchar('0' + j / 10);
+			putchar('0' + j % 10);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
