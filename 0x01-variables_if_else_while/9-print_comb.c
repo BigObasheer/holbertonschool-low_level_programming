d244c94e47d9c36f9eacd8ccbfa24f8b56e9f985
BigObasheer <838@holbertonschool.com>
2020-02-07 01:19:19 +0000

    Add Task 9]

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - single digit number combos
+ *
+ *Return: All possible number combos
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+
+	{
+		putchar(i);
+
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
