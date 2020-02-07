790902164ee2f7aa54511d575fcc411190b4b3a5
BigObasheer <838@holbertonschool.com>
2020-02-07 00:22:39 +0000

    Add Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (j = 'A'; j <= 'Z'; j++)
+		putchar(j);
+	putchar('\n');
+
+	return (0);
+}
