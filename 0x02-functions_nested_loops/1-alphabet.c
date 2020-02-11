c054442ba1133456d31f5d5b3a39f067cf608973
BigObasheer <838@holbertonschool.com>
2020-02-11 15:16:53 +0000

    Add task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <holberton.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+{
+	int x = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(y);
+	}
+	y++;
+	_putchar('\n');
+}
