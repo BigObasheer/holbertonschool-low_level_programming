a66e37a245be7ff699c2e5be217f25a5de9341bc
BigObasheer <838@holbertonschool.com>
2020-02-11 15:50:26 +0000

    Add 3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks for lower case letter
+ * @c: letter
+ * Return: 0 for uppercase 1 for lowercase
+ */
+
+int _islower(int c)
+
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+
+	else
+		return (0);
+}
