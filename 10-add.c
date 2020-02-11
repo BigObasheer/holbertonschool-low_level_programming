7af37f1e2db3830c9ec94ec9b7174b53483754a6
BigObasheer <838@holbertonschool.com>
2020-02-11 16:57:49 +0000

    Add task 10

---
+++ 10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
