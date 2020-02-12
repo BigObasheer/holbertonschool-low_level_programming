ce86b4864fa73e4bc960ab53499e164be32af31c
BigObasheer <838@holbertonschool.com>
2020-02-12 19:29:38 +0000

    Add Task 14

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,5 +6,24 @@
  *
  * Return: void
  */
+
 int main(void)
 {
+	long int i, j, k, l; sum = 0;
+
+	j = 1;
+	k = 2;
+
+	for (i = 0; i < 32; i++)
+	{
+		if (j % 2 == 0)
+		{
+			sum += j;
+		}
+		l = j + k;
+		j = k;
+		k = l;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
