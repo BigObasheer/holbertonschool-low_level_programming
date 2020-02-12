fa6a1a647c21f0aa81224d0f5606311315946793
BigObasheer <838@holbertonschool.com>
2020-02-12 18:44:34 +0000

    Add Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,5 +6,24 @@
  *
  * Return: void
  */
+
 int main(void)
 {
+	long int i, j, k, l;
+
+	j = 1;
+	k = 2;
+
+	for (i = 0; i < 50; i++)
+	{
+		if (i < 49)
+		{
+			printf("%ld, ", j);
+			l = j + k;
+			j = k;
+			k = l;
+		}
+		printf("%ld\n", j);
+	}
+	return (0);
+}
