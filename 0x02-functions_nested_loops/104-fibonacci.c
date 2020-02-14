5f19fa69a5ad645b8b5abcce5932ac270bc17229
BigObasheer <838@holbertonschool.com>
2020-02-14 17:14:05 +0000

    Add Task 14

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,3 +8,33 @@
  */
 int main(void)
 {
+	long int i, a, a1, a2, b, b1, b2, c, c1, c2;
+
+	a = 1;
+	b = 2;
+	a1 = a / 1000000;
+	a2 = a % 1000000;
+	b1 = b / 1000000;
+	b2 = b % 1000000;
+
+	for (i = 0; i < 98; i++)
+	{
+		if (i < 85)
+		{
+			printf("%ld, ", a);
+			c = a + b;
+			a = b;
+			b = c;
+		}
+		else if (i >= 85 && i < 97)
+		{
+			c1 = c / 1000000;
+			c2 = c % 1000000;
+		}
+		else
+		{
+			printf("%ld%ld\n", a1, a2);
+		}
+	}
+	return (0);
+}
