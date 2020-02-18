fa2a2afb38d0f13b80ef2f6f35727246017a6a5d
BigObasheer <838@holbertonschool.com>
2020-02-18 15:59:43 +0000

    Add Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,18 @@
 #include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		s++;
+	}
+	return(i);
+}
