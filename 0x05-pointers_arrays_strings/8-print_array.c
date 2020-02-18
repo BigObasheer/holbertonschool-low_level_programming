44c2b7541d5123820406e3b0c862772fef2d9cb5
BigObasheer <838@holbertonschool.com>
2020-02-18 19:43:14 +0000

    Add Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,24 @@
 #include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - function that prints every other character of a string,
+ * @a: array
+ * @n: number of elements in the array
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	putchar('\n');
+}
