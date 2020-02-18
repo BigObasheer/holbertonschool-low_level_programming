72974858a83afa18d891305c8ed94dfdafa8dac3
BigObasheer <838@holbertonschool.com>
2020-02-18 17:19:03 +0000

    Add Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,7 +12,18 @@ void puts2(char *str)
 
 	for (i = 0; i < (_strlen(str) - 1); i+=2)
 	{
-		_putchar(i);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
+}
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		s++;
+	}
+	return (i);
 }
