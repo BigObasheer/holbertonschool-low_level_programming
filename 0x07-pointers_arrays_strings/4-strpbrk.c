fd90621943a3b9d1991b104a9eb664d101346b54
BigObasheer <838@holbertonschool.com>
2020-02-24 20:56:21 +0000

    Add Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes.
+ * @s: string
+ * @accept: accept
+ * Return: pointer to the byte in s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i;
+
+	for (; *s != '\0'; s1++)
+	{
+		for (i = 0; accept[i]; i++)
+			if (*s == accept[i])
+			{
+				return (s1);
+			}
+	}
+	return (0);
+}
