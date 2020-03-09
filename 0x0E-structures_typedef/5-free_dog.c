732e1f5b1446f7b478d1056909c90431523e880d
BigObasheer <838@holbertonschool.com>
2020-03-09 22:09:29 +0000

    Add Task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dog - function that frees dog from memory
+ * @d: dog to free from memory
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
