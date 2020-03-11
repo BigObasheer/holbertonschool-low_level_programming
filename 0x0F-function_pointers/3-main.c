3bfe64cd11992f21ee8aa0343430ce8586c25952
BigObasheer <838@holbertonschool.com>
2020-03-11 22:50:26 +0000

    Add Task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+
+/**
+ * main - program that performs simple operations.
+ * @argc: arguement
+ * @argv: number of arguements
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, answer;
+	char *op = argv[2];
+	char *divide = "/";
+	char *mod = "%";
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if (b == 0 && (strcmp(op, divide) != 0 || strcmp(op, mod) != 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if ((*get_op_func(argv[2])) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	answer = (get_op_func(argv[2]))(a, b);
+	printf("%d\n", answer);
+	return (0);
+}
