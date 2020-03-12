248118e5d3ffbef59437b4892f59cf41de7562f7
BigObasheer <838@holbertonschool.com>
2020-03-12 03:39:28 +0000

    Add Task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+/**
+ * main - prints its own opcode
+ * @argc: number of arguments. Should ALWAYS be 2. Else, return ERROR.
+ * @argv: array of arguments.
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+}
