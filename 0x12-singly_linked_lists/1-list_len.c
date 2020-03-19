257cd7b0104212f4d529eabf505bbde09e465256
BigObasheer <838@holbertonschool.com>
2020-03-19 21:46:28 +0000

    Add Task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t
+ * list_list - returns the number of elements in a linked list_t list
  * @h: struct list_t, first node
  * Return: number of nodes
  */
 
-size_t print_list(const list_t *h)
+size_t list_list(const list_t *h)
 {
 	int i;
 
