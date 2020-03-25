c1fd7fc1b009ef35bd22bd7825c41fb10b20cd1b
BigObasheer <838@holbertonschool.com>
2020-03-25 02:43:58 +0000

    Add Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees memory allocated for a listint_t structure
+ * @head: head for listint_t struct
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (head != NULL && tmp != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
