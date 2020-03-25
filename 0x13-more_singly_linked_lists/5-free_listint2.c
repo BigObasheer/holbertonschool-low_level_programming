c5ac756c9fccd6c99fdf02c3fbd9a46d111de835
BigObasheer <838@holbertonschool.com>
2020-03-25 15:08:26 +0000

    Add Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	while (*head != NULL)
+	if (head != NULL)
 	{
-		tmp = *head;
-		*head = tmp->next;
-		free(tmp);
+		while (*head != NULL)
+		{
+			tmp = *head;
+			*head = tmp->next;
+			free(tmp);
+		}
 	}
 	free(*head);
 	*head = NULL;
