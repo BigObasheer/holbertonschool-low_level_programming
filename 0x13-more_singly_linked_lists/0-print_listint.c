0474709513d71d0978a68f1c4205a2a9e5a89de3
BigObasheer <838@holbertonschool.com>
2020-03-25 01:56:44 +0000

    Add Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,18 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t
+ * print_lisintt - prints all the elements of a listint_t
  * @h: struct list_t, first node
  * Return: number of nodes
  */
 
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
 	int i;
 
 	for (i = 0; h; i++, h = h->next)
 	{
-		if (h->str)
-			printf("[%i] %s\n", h->len, h->str);
-		else
-			printf("[0] (nil)\n");
+			printf("%i\n", h->n);
 	}
 	return (i);
 }
