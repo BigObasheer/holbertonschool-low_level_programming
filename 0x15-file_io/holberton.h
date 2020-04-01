ce0ddf94fe5f800d72cf03a285add2ee20f49deb
BigObasheer <838@holbertonschool.com>
2020-04-01 04:49:18 +0000

    Add header file

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -5,12 +5,15 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 
+#define BUFSIZE 1204
+
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void copy_textfile(char *filename, char *filecopy, int letters);
-int create_copyfile(const char *filename, char *text_content);
-int append_text_to_copyfile(const char *filename, char *text_content);
 
 #endif /*HOLBERTON_H*/
