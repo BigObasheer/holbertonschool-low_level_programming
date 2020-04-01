50c9c8647b20b98f801c915e82b88cfdd8855e77
BigObasheer <838@holbertonschool.com>
2020-04-01 04:37:09 +0000

    Add header file

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -12,5 +12,6 @@ int append_text_to_file(const char *filename, char *text_content);
 void copy_textfile(char *filename, char *filecopy, int letters);
 int create_copyfile(const char *filename, char *text_content);
 int append_text_to_copyfile(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
 
 #endif /*HOLBERTON_H*/
