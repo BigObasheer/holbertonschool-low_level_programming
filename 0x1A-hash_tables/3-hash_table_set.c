0bcd33160d53ece3b49ddac2129de64814eea5e9
BigObasheer <838@holbertonschool.com>
2020-10-02 20:40:42 +0000

    Add Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,16 +11,40 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *temp, *old;
 
-	if ((!ht) || (!key) || (!value))
+	if ((!ht) || (!key) ||  strcmp(key, "") == 0)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
+	for (temp = ht->array[index]; temp; temp = temp->next)
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
+		}
 	new_node = malloc(sizeof(hash_node_t));
-	new_node->key = (char *)key;
-	new_node->value = (char *)value;
-	ht->array[index] = new_node;
+
+	if (!new_node)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = strdup(value);
+	new_node->key = strdup(key);
+
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = new_node;
+		new_node->next = NULL;
+	}
+	else
+	{
+		old = ht->array[index];
+		new_node->next = old;
+		ht->array[index] = new_node;
+	}
 	return (1);
 }
