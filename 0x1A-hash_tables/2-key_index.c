f8da5cf25094bdb7d0a28bb61dd7ffcdf4349f3d
BigObasheer <838@holbertonschool.com>
2020-10-02 16:39:52 +0000

    Add Task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,9 +1,16 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - that gives yopu the index of a key
+ * @key: is the key
+ * @size: is the size of the array of the hash table
+ * Return: index of a key
+ */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
-	
+
 	if (!(key))
 	{
 		return (0);
