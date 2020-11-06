8dfabedd8cf024937f6edcc27ef9f3d94ecdeea5
BigOBasheer <838@holbertonschool.com>
2020-11-06 23:38:02 +0000

    Add Task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Returns perimeter of the island described """
+
+
+def island_perimeter(grid):
+    """ Return Perimeter """
+
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
